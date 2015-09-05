81e5b904ed43d5b026efb4119b59a0bc2f131886
Conny14156 <conny_nguyen94@hotmail.com>
2015-09-05 16:38:24 +0200

    Code Cleaning
    Moved Public to the top of the file, for the code standard

--- Project/Game/Component/MovementComponent.hpp
+++ Project/Game/Component/MovementComponent.hpp
@@ -6,12 +6,22 @@
 //Instantenous Movement, If accelreation is desired please use the appropriate component for it
 class MovementComponent: public IBaseComponent < MovementComponent >
 {
+
 public:
 
-	//Return true if the object is moving
+	Vector2 direction;
+	int distance;
+	double speed;
+	double rotation;
+	std::vector<std::pair<Vector2,double>> movementsQueue;
+
+public:
+
+	//Return true if the object is in motion (Ex: Displacement, Rotation)
 	bool inMotion();
 
 	void addForce(Vector2 direction,double speed);
+	//Replace the movement vectors
 	void replaceMotion(Vector2 direction,double speed,int distance);
 	void attachOn(GameObject* entity);
 
@@ -25,14 +35,6 @@ private:
 	Vector2 direction;
 	};
 	//*/
-public:
-
-	Vector2 direction;
-	int distance;
-	double speed;
-	double rotation;
-	std::vector<std::pair<Vector2,double>> forces;
-
 	//Move at parameter direction with s Speed
 	//void moveAt(Vector2 direction, double speed);
 	//void update();
