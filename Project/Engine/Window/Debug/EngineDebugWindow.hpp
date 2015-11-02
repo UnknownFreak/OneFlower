4929ebd67828e1d35bcfc6d86cefbb6d36e87e38
Conny14156 <conny_nguyen94@hotmail.com>
2015-11-02 19:07:09 +0100

    Revert "Revert "Quality of Life changes""
    This reverts commit 531d72d9e6d46e53d37cfb1d0168f71393d016d0.

--- Project/Engine/Window/Debug/EngineDebugWindow.hpp
+++ Project/Engine/Window/Debug/EngineDebugWindow.hpp
@@ -7,6 +7,7 @@ class EngineDebugMessage
 {
 public:
 	EngineDebugMessage(std::string msg);
+	~EngineDebugMessage();
 	std::string msg;
 	HWND hWnd;
 };
@@ -18,7 +19,7 @@ public:
 	std::vector<EngineDebugMessage> list;
 	//Type write __LINE__ as the second parameter and __FILE__ on third
 	void print(std::string msg,int __LINE___,std::string __FILE___);
-
+	void clear();
 private:
 	HWND hWnd;
 	int messageSize = 64;
