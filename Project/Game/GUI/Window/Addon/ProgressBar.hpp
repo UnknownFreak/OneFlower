d1695a2f7ad80b92d8c6af18639276f0c4840de9
UnknownFreak <skemro@gmail.com>
2015-12-11 00:01:26 +0100

    and for some reason added missing files...

---
+++ Project/Game/GUI/Window/Addon/ProgressBar.hpp
@@ -0,0 +1,52 @@
+#ifndef PROGRESSBAR_HPP
+#define PROGRESSBAR_HPP
+#include <SFML\Graphics\Sprite.hpp>
+#include "../../../../Vector.h"
+#include "../../ToolTip.hpp"
+#include "../../Text/Message.hpp"
+namespace GUI
+{
+	namespace Window
+	{
+		namespace Addon
+		{
+			class ProgressBar
+			{
+			private:
+
+				sf::VertexArray temporaryOutline;
+				sf::VertexArray temporaryBar;
+				int min;
+				int max;
+				int value;
+
+				bool isVertical;
+				bool showToolTip;
+
+				ToolTip toolTip;
+				Vector2 position;
+				Vector2 size;
+
+				Text::Message text;
+				sf::Sprite progressbar;
+			public:
+
+				//ProgressBar(int min, int max, int value, sf::Sprite progressbar,Text::Message text, Vector2 position, Vector2 size, bool isVertical, ToolTip tooltip = ToolTip("None"));
+				ProgressBar(int min, int max, int value, Vector2 pos, Vector2 size, bool isVertical);
+				void setProgressbarIcon(sf::Sprite icon);
+				void setSize(Vector2 size);
+				void setPosition(Vector2 position);
+				void setValue(int value);
+				void setMin(int min);
+				void setMax(int max);
+				void draw();
+				void setTooltip(ToolTip tooltip);
+				void setTooltip(std::string text);
+			};
+		}
+	}
+}
+
+
+
+#endif
