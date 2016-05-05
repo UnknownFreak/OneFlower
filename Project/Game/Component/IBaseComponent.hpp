7cd979ddce271dca21ffe252e7a7d2e018b6428b
Conny14156 <conny_nguyen94@hotmail.com>
2016-05-05 13:32:52 +0200

    Potentially Memory leak fix if Enabled
    Disabled for compiler reasons

--- Project/Game/Component/IBaseComponent.hpp
+++ Project/Game/Component/IBaseComponent.hpp
@@ -30,7 +30,16 @@ protected:
 	{
 		return editorFields;
 	}
-
+	/*
+	~IBaseComponent()
+	{
+		for(std::map<std::string,BaseField*>::iterator it = editorFields.begin(); it != editorFields.end(); ++it)
+		{
+			delete it->second;
+			it->second = 0;
+		}
+	}
+	//*/
 	//HIGH: Friendclass stuffz so you can put this into private
 	std::map<std::string,BaseField*> editorFields;
 #endif
