be8c7394418f1cb9bb7747549e42f76b01ae88cb
Conny14156 <conny_nguyen94@hotmail.com>
2016-05-05 13:28:07 +0200

    Vector inlined
    Vector inlined and disabled definer

--- Project/Vector.h
+++ Project/Vector.h
@@ -1,40 +1,67 @@
 #ifndef TemplateVector
 #define TemplateVector
 #include <ostream>
-struct Vector2
+template <typename T>
+struct Vector2t
 {
-	Vector2();
-	Vector2(int a,int b);
+	Vector2t<T>();
+	//Vector2(int a,int b);
+	Vector2t<T>(T a,T b);
 	//virtual ~Vector2() = default;
-	double x;
-	double y;
+	T x;
+	T y;
 	//LOW: Make it so that normalize returns a normal vector instead 2015/13/02
 	//Normalize the vector to get a direction NOT Reverseable
 	bool normalize();
-
+	double magnitude();
 	//LOW make it when a vector2 is reassigned it is no longer normalized
-	bool normalized = false;
+	//bool isNormalize();// = false;
+
+
+	//The angle between this and target
+	double angle(Vector2t<T> * target);
+
 
 	//LOW: Make a overload with primitive parameters (Int double etc);
 	//The distance between this and target
-	double distance(Vector2* target);
+	double distance(Vector2t<T> * target);
 
-	//The angle between this and target
-	double angle(Vector2* target);
+
 
 	//The Direction between this and target
-	Vector2 direction(Vector2& target);
+	Vector2t<T> direction(Vector2t<T> & target);
 
-#pragma region Operator
-	friend std::ostream & operator<< (std::ostream &out,Vector2 const &t);
-	void operator += (Vector2& right);
-	void operator -= (Vector2& right);
-	void operator *= (Vector2& right);
-
-	//TODO: Make these inline cause primitive covert type, V.x + Left.x while the other is assignment operator (+= etc)
-	Vector2 operator - (Vector2 left);
-	Vector2 operator + (Vector2 right);
-#pragma endregion
 };
+#pragma region Operator
+template <typename T>
+std::ostream & operator<< (std::ostream &out,Vector2t<T>  const &t);
+
+//& instead of void, for copy/move sesmatic optimization
+
+template <typename T>
+Vector2t<T> & operator *= (Vector2t<T>& left,const Vector2t<T> & right);
+
+template <typename T>
+Vector2t<T> & operator += (Vector2t<T>& left,const Vector2t<T> & right);
+
+template <typename T>
+Vector2t<T> & operator -= (Vector2t<T>& left,const Vector2t<T> & right);
+
+//TODO: Make these inline cause primitive covert type, V.x + Left.x while the other is assignment operator (+= etc)
+template <typename T>
+Vector2t<T>  operator - (const Vector2t<T>& left,const Vector2t<T>  right);
+
+template <typename T>
+Vector2t<T>  operator + (const Vector2t<T>& left,const Vector2t<T>  right);
+
+template <typename T>
+Vector2t<T>  operator * (const Vector2t<T>& left,T right);
+//*/
+#pragma endregion
+
+#include "Vector.inl"
+typedef Vector2t<double> Vector2;
+typedef Vector2t<int> Vector2i;
+
 
 #endif
--- Project/VectorDefiner.cpp
+++ Project/VectorDefiner.cpp
@@ -1,98 +1,139 @@
-#include "Vector.h"
 #include <cmath>
-#include <Windows.h>
-#include "Engine.hpp"
-Vector2::Vector2()
+/*
+template<typename T>
+inline Vector2t<T>::Vector2t():x(0),y(0)
 {
-	x = 0;
-	y = 0;
-}
-Vector2::Vector2(int a,int b)
-{
-	x = a;
-	y = b;
-}
-double Vector2::distance(Vector2* target)
-{
-	double px = ((target->x - x) * (target->x - x));
-	double py = ((target->y - y) * (target->y - y));
 
-	return  std::sqrt(px + py);
 }
-
-double Vector2::angle(Vector2* target)
+//*/
+/*
+Vector2t<T>::Vector2(int a,int b)
 {
-	double deltaX = target->x - x;
-	double deltaY = target->y - y;
-
-	int angleInDegree = std::atan2(deltaY,deltaX) * 180 / 3.14159265358979323846;
-	return angleInDegree;
+x = a;
+y = b;
 }
-Vector2 Vector2::direction(Vector2& target)
+//*/
+/*
+template<typename T>
+inline Vector2t<T>::Vector2(T a,T b):x(a),y(b)
 {
-	Vector2 a(target.x - x,target.y - y);
-	a.normalize();
 
-	//LOW: use operator
-	return a;
+}
+template<typename T>
+inline double Vector2t<T>::distance(Vector2* target)
+{
+double px = ((target->x - x) * (target->x - x));
+double py = ((target->y - y) * (target->y - y));
+
+return  std::sqrt(px + py);
 }
 
-bool Vector2::normalize()
+template<typename T>
+inline double Vector2t<T>::angle(Vector2* target)
 {
-	float len = sqrt(x*x + y*y);
-	if(len != 0)
-	{
-		x = x / len;
-		y = y / len;
-		normalized = true;
-		return true;
-	}
-	else
-	{ 
+double deltaX = target->x - x;
+double deltaY = target->y - y;
+
+int angleInDegree = std::atan2(deltaY,deltaX) * 180 / 3.14159265358979323846;
+return angleInDegree;
+}
+template<typename T>
+inline Vector2t<T> Vector2t<T>::direction(Vector2t<T> target)
+{
+Vector2t<T> a(target.x - x,target.y - y);
+a.normalize();
+
+//LOW: use operator
+return a;
+}
+template<typename T>
+inline double Vector2t<T>::magnitude()
+{
+return sqrt(x*x + y*y);
+}
+template<typename T>
+inline bool Vector2t<T>::normalize()
+{
+float len = magnitude();
+//Avoid divide with 0
+if(len != 0)
+{
+x = x / len;
+y = y / len;
+return true;
+}
+else
+{
 #ifdef _DEBUG
-		Engine::Window.debug.print("len = 0 Vector2 Normalize failed, Error: Vector2Norm",__LINE__,__FILE__);
+//Engine::Window.debug.print("len = 0 Vector2t<T> Normalize failed, Error: Vector2Norm",__LINE__,__FILE__);
 #endif
-		//*/
-		return false;
-	}
-	//LOW: Fix this someday and be sure to check some math site this time to. 15/02/13
+return false;
+}
+//LOW: Fix this someday and be sure to check some math site this time to. 15/02/13
 }
 
+/*
 #include <ostream>
-std::ostream & operator<< (std::ostream &out,Vector2 const &t)
+std::ostream & operator<< (std::ostream &out,Vector2t<T> const &t)
 {
-	out << t.x << t.y;
-	return out;
+out << t.x << t.y;
+return out;
 }
+//*/
 
-void Vector2::operator+=(Vector2& right)
+#pragma region Operators
+/*
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator+=(Vector2t<T>& left,const Vector2t<T> right)
 {
-	x += right.x;
-	y += right.y;
+left.x += right.x;
+left.y += right.y;
+return *this;
 }
-void Vector2::operator-=(Vector2& right)
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator-=(Vector2t<T>& left,const Vector2t<T> right)
 {
-	x -= right.x;
-	y -= right.y;
-}
-void Vector2::operator*=(Vector2& right)
-{
-	x *= right.x;
-	y *= right.y;
-}
-Vector2 Vector2::operator-(Vector2 right)
-{
-	Vector2 left(x,y);
-	left.x -= right.x;
-	left.y -= right.y;
+left.x -= right.x;
+left.y -= right.y;
+return *this;
 
-	return left;
 }
-Vector2 Vector2::operator+(Vector2 right)
+//*/
+/*
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator*=(Vector2t<T>& left,const Vector2t<T> right)
 {
-	Vector2 left(x,y);
-	left.x += right.x;
-	left.y += right.y;
+left.x *= right.x;
+left.y *= right.y;
+return *this;
 
-	return left;
 }
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator-(Vector2t<T>& left,const Vector2t<T> right)
+{
+//Vector2t<T> left(x,y);
+left.x -= right.x;
+left.y -= right.y;
+
+return left;
+}
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator+(Vector2t<T>& left,const Vector2t<T> right)
+{
+//Vector2t<T> left(x,y);
+left.x += right.x;
+left.y += right.y;
+
+return left;
+}
+template<typename T>
+inline Vector2t<T> Vector2t<T>::operator*(Vector2t<T>& left,double right)
+{
+//Vector2t<T> left(x,y);
+left.x *= right;
+left.y *= right;
+
+return left;
+}
+//*/
+#pragma endregion
