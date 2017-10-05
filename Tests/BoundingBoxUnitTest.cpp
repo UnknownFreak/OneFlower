671b74daf1e58961e42624a5272b681c32595da6
UnknownFreak <skemro@gmail.com>
2017-10-05 16:55:45 +0200

    Added more unit tests to check no collide as well

--- Tests/BoundingBoxUnitTest.cpp
+++ Tests/BoundingBoxUnitTest.cpp
@@ -31,27 +31,52 @@ namespace Tests
 		TEST_METHOD(TestBoundingBoxCollideTopLeft)
 		{
 			Physics::BoundingBox a(0, 0, 10, 10);
-			Physics::BoundingBox b(-9, -9, 10, 10);
+			Physics::BoundingBox b(-10, -10, 10, 10);
 			Assert::IsTrue(a.intersect(b));
 		}
 		TEST_METHOD(TestBoundingBoxCollideBottomLeft)
 		{
 			Physics::BoundingBox a(0, 0, 10, 10);
-			Physics::BoundingBox b(-9, 9, 10, 10);
+			Physics::BoundingBox b(-10, 10, 10, 10);
 			Assert::IsTrue(a.intersect(b));
 
 		}
 		TEST_METHOD(TestBoundingBoxCollideTopRight)
 		{
 			Physics::BoundingBox a(0, 0, 10, 10);
-			Physics::BoundingBox b(9, -9, 10, 10);
+			Physics::BoundingBox b(10, -10, 10, 10);
 			Assert::IsTrue(a.intersect(b));
 		}
 		TEST_METHOD(TestBoundingBoxCollideBottomRight)
 		{
 			Physics::BoundingBox a(0, 0, 10, 10);
-			Physics::BoundingBox b(9, 9, 10, 10);
+			Physics::BoundingBox b(10, 10, 10, 10);
 			Assert::IsTrue(a.intersect(b));
+		}
+		TEST_METHOD(TestBoundingBoxNoCollideTopLeft)
+		{
+			Physics::BoundingBox a(0, 0, 10, 10);
+			Physics::BoundingBox b(-10, -10, 9, 9);
+			Assert::IsFalse(a.intersect(b));
+		}
+		TEST_METHOD(TestBoundingBoxNoCollideBottomLeft)
+		{
+			Physics::BoundingBox a(0, 0, 10, 10);
+			Physics::BoundingBox b(-10, 10, 9, 9);
+			Assert::IsFalse(a.intersect(b));
+
+		}
+		TEST_METHOD(TestBoundingBoxNoCollideTopRight)
+		{
+			Physics::BoundingBox a(0, 0, 10, 10);
+			Physics::BoundingBox b(10, -10, 9, 9);
+			Assert::IsFalse(a.intersect(b));
+		}
+		TEST_METHOD(TestBoundingBoxNoCollideBottomRight)
+		{
+			Physics::BoundingBox a(0, 0, 10, 10);
+			Physics::BoundingBox b(11, 11, 9, 9);
+			Assert::IsFalse(a.intersect(b));
 		}
 	};
 
