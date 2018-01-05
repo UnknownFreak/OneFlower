de43ad85c384ed5218b532f3c05bc456f4bb847d
UnknownFreak <skemro@gmail.com>
2018-01-05 19:38:49 +0100

    Updated GraphicsCore to work with unit tests.

--- Project/Graphic/GraphicsCore.hpp
+++ Project/Graphic/GraphicsCore.hpp
@@ -4,11 +4,12 @@
 #include "Gfx.h"
 #include <AssetManager\AssetManagerCore.hpp>
 #include "Time\Time.hpp"
+
+#ifndef _UNITTESTS_
 namespace Engine
 {
 	extern sf::Event event;
-
 }
-
+#endif
 
 #endif
