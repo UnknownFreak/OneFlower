b53fffef3fd421fa8e4dc1302f36f9abfacde263
UnknownFreak <skemro@gmail.com>
2018-02-20 17:02:53 +0100

    New class primitive saveable.
    Saves primitive types (int, double, string, etc.)
    This commit does not update the filters and project file.

---
+++ Project/AssetManager/PrimitiveSaveable.hpp
@@ -0,0 +1,56 @@
+#ifndef PrimitiveSaveable_HPP
+#define PrimitiveSaveable_HPP
+
+#include "IRequestable.hpp"
+
+template <class T>
+class PrimitiveSaveable : public IRequestable
+{
+public:
+	T value;
+	Core::String name;
+
+	inline PrimitiveSaveable() : IRequestable("", 0, OneVersion(1, 0, 0)), value(), name()
+	{
+	}
+
+	inline PrimitiveSaveable(T value, Core::String name) : PrimitiveSaveable(value, name, "", 0, OneVersion(1, 0, 0))
+	{
+	}
+
+	inline PrimitiveSaveable(T value, Core::String name, const Core::String fromMod, const size_t ID, const OneVersion version) : IRequestable(fromMod, ID, version), value(value), name(name)
+	{
+	}
+
+	inline PrimitiveSaveable(const PrimitiveSaveable& copy) : IRequestable(copy), value(copy.value), name(copy.name)
+	{
+	}
+
+	inline PrimitiveSaveable& operator=(const PrimitiveSaveable& right)
+	{
+		value = right.value;
+		name = right.name;
+		fromMod = right.fromMod;
+		ID = right.ID;
+		mode = right.mode;
+		objectVersion = right.objectVersion;
+		return *this;
+	}
+
+	template <class Archive>
+	void save(Archive& ar) const
+	{
+		ar(cereal::base_class<IRequestable>(this));
+		ar(value);
+		ar(name);
+	}
+
+	template <class Archive>
+	void load(Archive& ar)
+	{
+		ar(cereal::base_class<IRequestable>(this));
+		ar(value);
+		ar(name);
+	}
+};
+#endif
