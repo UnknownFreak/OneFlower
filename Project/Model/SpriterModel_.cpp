e707e7c1c92574a65b310035039fda4d82ae576f
UnknownFreak <skemro@gmail.com>
2018-02-20 17:39:52 +0100

    Added missing files x2
    Derpy git is derpy

--- Project/Model/SpriterModel_.cpp
+++ Project/Model/SpriterModel_.cpp
@@ -2,8 +2,36 @@
 #include <Graphic\Time\Time.hpp>
 namespace Model
 {
-	Model::SpriterModel::SpriterModel()
 	{
+	}
+
+	SpriterModel::SpriterModel() : myEntity(nullptr)
+	{
+		objectVersion = OneVersion(1, 0, 0);
+	}
+
+	SpriterModel::~SpriterModel()
+	{
+	}
+
+	SpriterModel::SpriterModel(const SpriterModel & copy) : IModel(copy), fileName(copy.fileName), modelNames(copy.modelNames), selectedModelName(copy.selectedModelName),
+		textureMapId(textureMapId)
+	{
+	}
+
+	SpriterModel & SpriterModel::operator=(const SpriterModel & right)
+	{
+		if (this == &right)
+			return *this;
+
+		ID = right.ID;
+		fromMod = right.fromMod;
+		mode = right.mode;
+		objectVersion = right.objectVersion;
+
+		m_render_sprite = right.m_render_sprite;
+		updateRequired = right.updateRequired;
+		return *this;
 	}
 	void SpriterModel::updateFrame(const size_t& update_ms)
 	{
