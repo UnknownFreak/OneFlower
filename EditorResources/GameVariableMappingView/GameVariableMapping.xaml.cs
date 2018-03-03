00813b92dc0e0b8b0f76429b49cb94693dbdb280
UnknownFreak <skemro@gmail.com>
2018-03-03 14:36:27 +0100

    Updated csproj and added new files.

---
+++ EditorResources/GameVariableMappingView/GameVariableMapping.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using EditorResources.Functionality;
+using static EditorResources.Functionality.EngineEvents;
+using static EditorResources.Functionality.RequestEvents;
+
+namespace EditorResources.GameVariableMappingView
+{
+    /// <summary>
+    /// Interaction logic for GameVariableMapping.xaml
+    /// </summary>
+    public partial class GameVariableMapping : Window
+    {
+        public GameVariableMapping()
+        {
+            InitializeComponent();
+            RequestEvents.OnRequestGameVariableMappingVariable(new GameVariableMappingEventArgs() {VariableToRequest = GameVariableMappingEventArgs.Variable.Animation});
+            RequestEvents.OnRequestGameVariableMappingVariable(new GameVariableMappingEventArgs() {VariableToRequest = GameVariableMappingEventArgs.Variable.MainMenu});
+            RequestEvents.OnRequestGameVariableMappingVariable(new GameVariableMappingEventArgs() {VariableToRequest = GameVariableMappingEventArgs.Variable.StartingZone});
+
+            EngineEvents.engineOnSendVariableMapping += enigineOnSendAnimationVariableSubscriber;
+        }
+
+        private void enigineOnSendAnimationVariableSubscriber(object sender, VariableMappingEventArgs e)
+        {
+            if (e.GetType() == typeof(AnimationVariableMappingEventArgs))
+                assignAnimationVariable(e as AnimationVariableMappingEventArgs);
+        }
+
+        private void assignAnimationVariable(AnimationVariableMappingEventArgs args)
+        {
+            AnimationVariableName.Text = args.AnimationVariable;
+        }
+    }
+}
