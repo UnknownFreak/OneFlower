84af82d8458f8a87241d4ca0d90b1d20620cec25
UnknownFreak <skemro@gmail.com>
2018-10-28 14:49:37 +0100

    Add languageview to manage language strings.

---
+++ EditorResources/Windows/LanguageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace EditorResources.Windows
+{
+    /// <summary>
+    /// Interaction logic for LanguageView.xaml
+    /// </summary>
+    public partial class LanguageView : Window
+    {
+        public LanguageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
