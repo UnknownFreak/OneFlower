381788a3719e5c12eba8a2e04ee46a50b755c749
UnknownFreak <skemro@gmail.com>
2018-11-25 21:23:11 +0100

    Add a new window to confirm object removal.

---
+++ EditorResources/Windows/RemoveObjectConfirmBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace EditorResources.Windows
+{
+    /// <summary>
+    /// Interaction logic for RemoveObjectConfirmBox.xaml
+    /// </summary>
+    public partial class RemoveObjectConfirmBox : Window
+    {
+        private bool Result { get; set; } = false;
+        private string deleteString = "Are you sure you want to delete {objectName}?";
+
+        private RemoveObjectConfirmBox() : this("")
+        {
+        }
+
+        private RemoveObjectConfirmBox(string objectToDeleteName)
+        {
+            InitializeComponent();
+            box.Text = string.Format(deleteString, objectToDeleteName);
+        }
+
+        public static bool ShowAndGetResult(string objectName)
+        {
+            RemoveObjectConfirmBox box = new RemoveObjectConfirmBox(objectName);
+            box.ShowDialog();
+            return box.Result;
+        }
+
+        private void Yes_click(object sender, RoutedEventArgs e)
+        {
+            Result = true;
+            Close();
+        }
+
+        private void No_click(object sender, RoutedEventArgs e)
+        {
+            Result = false;
+            Close();
+        }
+    }
+}
