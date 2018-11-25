e4c4bc693349ac58fe8e967714e40398ecd79d73
UnknownFreak <skemro@gmail.com>
2018-11-25 21:55:14 +0100

    Use correct field names for LanguageStringDto

--- EditorResources/Dto/LanguageString.cs
+++ EditorResources/Dto/LanguageString.cs
@@ -1,8 +1,16 @@
 ﻿namespace EditorResources.Dto
 {
-    public class LanguageString : BaseDto
+    public class LanguageStringDto : BaseDto
     {
-        public string Value;
-        public string LanguageId;
+        public string Value { get; set; }
+        public string Filename { get; set; }
+        public string Language { get; set; }
+    }
+
+    public class LanguageFileDto : BaseDto
+    {
+        public string Filename { get; set; }
+
+        public override string ToString() => Filename;
     }
 }
