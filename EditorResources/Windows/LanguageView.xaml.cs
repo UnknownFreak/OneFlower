796fb4935caadc6cf518fa8974f4952c29e95d34
UnknownFreak <skemro@gmail.com>
2018-11-25 21:24:10 +0100

    Update LanguageView.

--- EditorResources/Windows/LanguageView.xaml.cs
+++ EditorResources/Windows/LanguageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using EditorResources.Utils;
+using System.Windows;
 
 namespace EditorResources.Windows
 {
@@ -10,6 +11,36 @@ namespace EditorResources.Windows
         public LanguageView()
         {
             InitializeComponent();
+        }
+
+        private void AddLanguageFile_Click(object sender, RoutedEventArgs e)
+        {
+            string s;
+            if ((s = AddObjectNameBox.ShowAndGetResult()) != string.Empty)
+            {
+                Dto.LanguageFileDto dto = new Dto.LanguageFileDto() {Filename=s, Name="",Origin="NotUsed"};
+                EditorEvents.OnObjectEvent(new EditorEvents.ObjectEventArgs()
+                {
+                    Flag = EnumCollection.ObjectFlag.Added,
+                    Type = EnumCollection.ObjectType.TranslationFile,
+                    Value = dto
+                });
+                LanguageFileList.Items.Add(dto);
+            }
+        }
+
+        private void RemoveLanguageFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (LanguageFileList.SelectedItem != null && RemoveObjectConfirmBox.ShowAndGetResult(LanguageFileList.SelectedItem.ToString()))
+            {
+                EditorEvents.OnObjectEvent(new EditorEvents.ObjectEventArgs()
+                {
+                    Flag = EnumCollection.ObjectFlag.Deleted,
+                    Type = EnumCollection.ObjectType.TranslationFile,
+                    Value = LanguageFileList.SelectedItem as Dto.LanguageStringDto
+                });
+                LanguageFileList.Items.Remove(LanguageFileList.SelectedItem);
+            }
         }
     }
 }
