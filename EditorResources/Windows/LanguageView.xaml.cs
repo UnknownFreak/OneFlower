0db9fcda3783a6975b409da0e884b4830d4fc02e
UnknownFreak <skemro@gmail.com>
2018-12-30 18:53:27 +0100

    Bugfix on removing tanslationString

--- EditorResources/Windows/LanguageView.xaml.cs
+++ EditorResources/Windows/LanguageView.xaml.cs
@@ -133,7 +133,7 @@ namespace EditorResources.Windows
                     Type = EnumCollection.ObjectType.TranslationString,
                     Value = translationStringList.SelectedItem as Dto.LanguageStringDto
                 });
-                LanguageFileList.Items.Remove(LanguageFileList.SelectedItem);
+                translationStringList.Items.Remove(LanguageFileList.SelectedItem);
             }
         }
 
