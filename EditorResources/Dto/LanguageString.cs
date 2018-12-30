0a110b83b2f6ffb5a256a7af862e7c387eb6b485
UnknownFreak <skemro@gmail.com>
2018-12-30 18:50:08 +0100

    Add ToString override for LanguaageStringDto

--- EditorResources/Dto/LanguageString.cs
+++ EditorResources/Dto/LanguageString.cs
@@ -5,6 +5,8 @@
         public string Value { get; set; }
         public string Filename { get; set; }
         public string Language { get; set; }
+
+        public override string ToString() => $"Value={Value}, Filename={Filename}, Language={Language}";
     }
 
     public class LanguageFileDto : BaseDto
