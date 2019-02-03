cfd04bddc35a8671a49a44bfa0b97e2d3e7061a7
UnknownFreak <skemro@gmail.com>
2019-02-03 13:30:23 +0100

    Add overrides for Origin and Name for LanguageString & LanguageFile Dto.

--- EditorResources/Dto/LanguageString.cs
+++ EditorResources/Dto/LanguageString.cs
@@ -5,6 +5,10 @@
         public string Value { get; set; }
         public string Filename { get; set; }
         public string Language { get; set; }
+        public string Font { get; set; }
+
+        public new string Origin { get { return Filename; } set { Filename = value; } }
+        public new string Name { get { return Value; } set { Value = value; } }
 
         public override string ToString() => $"Value={Value}, Filename={Filename}, Language={Language}";
     }
@@ -12,6 +16,8 @@
     public class LanguageFileDto : BaseDto
     {
         public string Filename { get; set; }
+
+        public new string Origin { get { return Filename; } set { Filename = value; } }
 
         public override string ToString() => Filename;
     }
