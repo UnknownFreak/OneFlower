6aa4e2eac784c363d6586295a01787fcc99d7885
UnknownFreak <UnknownFreak@users.noreply.github.com>
2022-02-10 19:11:59 +0100

    Why did these not get added...

--- Project/Helpers/uuid.hpp
+++ Project/Helpers/uuid.hpp
@@ -1,14 +1,15 @@
 #ifndef UUID_HPP
 #define UUID_HPP
-#include <cereal/types/string.hpp>
 #include <uuid.h>
-#include <Core/String.hpp>
+#include <Helpers/String.hpp>
+#include <vector>
+
 namespace Core
 {
 	class uuid
 	{
 		uuids::uuid m_uuid;
-
+	
 		inline uuids::uuid from_string(const Core::String& str) noexcept
 		{
 			if (uuids::uuid::is_valid_uuid(str))
@@ -18,7 +19,7 @@ namespace Core
 			else
 				return uuids::uuid_system_generator{}();
 		}
-
+	
 	public:
 		inline uuid() noexcept : m_uuid(uuids::uuid_system_generator{}())
 		{}
@@ -26,72 +27,89 @@ namespace Core
 		{}
 		inline uuid(const uuids::uuid& uuid) noexcept : m_uuid(uuid)
 		{}
-		inline uuid(const uuid&& uuid) noexcept : m_uuid(std::move(uuid.m_uuid))
+		inline uuid(const uuid&& uuid) noexcept : m_uuid(uuid.m_uuid)
 		{}
 		inline uuid(const Core::String& str) noexcept : m_uuid(from_string(str))
+		{}
+	
+		static uuid nil() noexcept
 		{
+			return uuid(uuids::uuid{});
 		}
-
-		static uuid nil()
-		{
-			uuids::uuid id; 
-			return uuid(id);
-		}
-
+	
 		inline constexpr bool is_nil() const noexcept
 		{
 			return m_uuid.is_nil();
 		}
-
+	
 		inline uuid& operator=(const uuid& uuid)
 		{
 			m_uuid = uuid.m_uuid;
 			return *this;
 		}
-
+	
 		template<class Archive>
 		void save(Archive& ar) const
 		{
-			ar(uuids::to_string(m_uuid));
+			const size_t length = m_uuid.as_bytes().size();
+			const std::byte* data = m_uuid.as_bytes().data();
+			for (size_t i = 0; i < length; i++)
+				ar(*(data + i));
 		}
-
+	
 		template<class Archive>
 		void load(Archive& ar)
 		{
-			std::string s;
-			ar(s);
-			m_uuid = from_string(s);
+			const size_t length = m_uuid.as_bytes().size();
+			std::vector<uint8_t> byte_vec;
+			std::uint8_t b;
+			for (size_t i = 0; i < length; i++)
+			{
+				ar(b);
+				byte_vec.push_back(b);
+			}
+			m_uuid = uuids::uuid(byte_vec.begin(), byte_vec.end());
 		}
-
+	
 		inline std::string to_string() const
 		{
 			std::stringstream sstr;
 			sstr << this->m_uuid;
 			return sstr.str();
 		}
-
+	
 		inline std::wstring to_wstring() const
 		{
 			std::wstringstream sstr;
 			sstr << this->m_uuid;
 			return sstr.str();
 		}
-
+	
 		inline bool operator<(const Core::uuid& right) const noexcept
 		{
 			return m_uuid < right.m_uuid;
 		}
-
-		inline bool operator!=(const Core::uuid& right)const noexcept
+	
+		inline bool operator!=(const Core::uuid& right) const noexcept
 		{
 			return m_uuid != right.m_uuid;
 		}
-
-		inline bool operator==(const Core::uuid& right)const noexcept
+	
+		inline bool operator==(const Core::uuid& right) const noexcept
 		{
 			return m_uuid == right.m_uuid;
 		}
 	};
 }
-
+namespace std
+{
+	template <>
+	struct hash<Core::uuid>
+	{
+		std::size_t operator()(Core::uuid const& uuid) const
+		{
+			return std::hash<std::string>()(uuid.to_string());
+		}
+	};
+}
 #endif
