b9fd4996def3a34b6443ff88a0158dd85a85a368
UnknownFreak <UnknownFreak@users.noreply.github.com>
2022-10-16 14:08:01 +0200

    Update swizzle with bugfix for crash on minimize.

--- Project/Module/BuildVersion.hpp
+++ Project/Module/BuildVersion.hpp
@@ -5,7 +5,7 @@
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
 #define VERSION_REV   1
-#define VERSION_BUILD 1770
+#define VERSION_BUILD 1771
 
 #define stringify(x) _stringify(x)
 #define _stringify(x) #x
