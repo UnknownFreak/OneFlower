3454366ca0905597b6babfe592a11965d96c86a1
UnknownFreak <UnknownFreak@users.noreply.github.com>
2022-10-18 19:14:08 +0200

    Update build version

--- Project/Module/BuildVersion.hpp
+++ Project/Module/BuildVersion.hpp
@@ -5,7 +5,7 @@
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
 #define VERSION_REV   1
-#define VERSION_BUILD 1891
+#define VERSION_BUILD 1892
 
 #define stringify(x) _stringify(x)
 #define _stringify(x) #x
