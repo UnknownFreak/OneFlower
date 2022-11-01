73edcf61353bd205c03695aa675fabf1f2865177
UnknownFreak <UnknownFreak@users.noreply.github.com>
2022-11-01 17:24:05 +0100

    Update swizzle submodule.

--- Project/Module/BuildVersion.hpp
+++ Project/Module/BuildVersion.hpp
@@ -5,7 +5,7 @@
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
 #define VERSION_REV   1
-#define VERSION_BUILD 2276
+#define VERSION_BUILD 2277
 
 #define stringify(x) _stringify(x)
 #define _stringify(x) #x
