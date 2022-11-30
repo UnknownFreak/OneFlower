ccf2307832d3d9974f06cabc95ea2e0e0bf81c9c
UnknownFreak <UnknownFreak@users.noreply.github.com>
2022-11-30 19:38:43 +0100

    Add swizzle gfx proxy

---
+++ OneFlowerLibs/swizzle-gfx-proxy/include/module/window/GraphicsProxy.hpp
@@ -0,0 +1,38 @@
+#ifndef GraphicsProxy_HPP
+#define GraphicsProxy_HPP
+
+#include <memory>
+
+#include <module/IEngineModule.hpp>
+#include <swizzle/gfx/Context.hpp>
+#include <swizzle/gfx/CommandBuffer.hpp>
+#include <swizzle/gfx/Swapchain.hpp>
+
+
+namespace of::module::window
+{
+	class Proxy : public interface::IEngineResource<Proxy>
+	{
+	private:
+
+		std::shared_ptr<swizzle::gfx::GfxContext> m_gfxContext;
+		std::shared_ptr<swizzle::gfx::CommandBuffer> m_cmdBuffer;
+		std::shared_ptr<swizzle::gfx::CommandBuffer> m_uploadBuffer;
+		std::shared_ptr<swizzle::gfx::Swapchain> m_swapChain;
+	public:
+
+		Proxy();
+
+		void setProxy(std::shared_ptr<swizzle::gfx::GfxContext> gfx, std::shared_ptr<swizzle::gfx::CommandBuffer> cmdBuf, std::shared_ptr<swizzle::gfx::CommandBuffer> upload,
+			std::shared_ptr<swizzle::gfx::Swapchain> swapChain);
+
+		std::shared_ptr<swizzle::gfx::GfxContext> getGfxContext();
+		std::shared_ptr<swizzle::gfx::CommandBuffer> getCommandBuffer();
+		std::shared_ptr<swizzle::gfx::CommandBuffer> getUploadBuffer();
+		std::shared_ptr<swizzle::gfx::Swapchain> getSwapchain();
+
+		EngineResourceType& getType() const override;
+	};
+}
+
+#endif
---
+++ OneFlowerLibs/swizzle-gfx-proxy/src/GraphicsProxy.cpp
@@ -0,0 +1,37 @@
+#include <module/window/GraphicsProxy.hpp>
+
+of::module::EngineResourceType of::module::interface::IEngineResource<of::module::window::Proxy>::type = of::module::EngineResourceType::WindowProxy;
+
+
+namespace of::module::window
+{
+	Proxy::Proxy() {}
+
+	void Proxy::setProxy(std::shared_ptr<swizzle::gfx::GfxContext> gfx, std::shared_ptr<swizzle::gfx::CommandBuffer> cmdBuf, std::shared_ptr<swizzle::gfx::CommandBuffer> upload,
+		std::shared_ptr<swizzle::gfx::Swapchain> swapChain)
+	{
+		m_gfxContext = gfx;
+		m_cmdBuffer = cmdBuf;
+		m_uploadBuffer = upload;
+		m_swapChain = swapChain;
+	}
+
+	std::shared_ptr<swizzle::gfx::GfxContext> Proxy::getGfxContext() {
+		return m_gfxContext;
+	}
+	std::shared_ptr<swizzle::gfx::CommandBuffer> Proxy::getCommandBuffer() {
+		return m_cmdBuffer;
+	}
+	std::shared_ptr<swizzle::gfx::CommandBuffer> Proxy::getUploadBuffer() {
+		return m_uploadBuffer;
+	}
+	std::shared_ptr<swizzle::gfx::Swapchain> Proxy::getSwapchain()
+	{
+		return m_swapChain;
+	}
+
+	EngineResourceType& Proxy::getType() const
+	{
+		return type;
+	}
+};
