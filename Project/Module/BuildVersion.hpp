81f949feb45a7eacbb758908c8cf92a0b1dd7212
UnknownFreak <UnknownFreak@users.noreply.github.com>
2023-07-30 15:32:41 +0200

    Update swizzle

--- Project/Module/BuildVersion.hpp
+++ Project/Module/BuildVersion.hpp
@@ -5,7 +5,7 @@
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
 #define VERSION_REV   1
-#define VERSION_BUILD 4852
+#define VERSION_BUILD 4859
 
 #define stringify(x) _stringify(x)
 #define _stringify(x) #x
