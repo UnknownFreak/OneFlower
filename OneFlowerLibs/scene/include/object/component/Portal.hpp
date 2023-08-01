bff7c717a62e7ecfba5ea2df4669d3e0326ae18f
UnknownFreak <UnknownFreak@users.noreply.github.com>
2023-08-01 22:09:55 +0200

    Portal poc created

---
+++ OneFlowerLibs/scene/include/object/component/Portal.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <object/component/BaseComponent.hpp>
+
+#include <module/sceneManager.hpp>
+
+namespace of::object::component
+{
+	class Portal : public Base
+	{
+		
+		virtual void onMessage(const of::object::messaging::Message& message) override {};
+		virtual void initialize() override;
+		virtual void deconstruct() override;
+
+		of::file::FileId world;
+		of::file::FileId loadingScreen;
+		// change pos coordinates to player pos marker object, that once the zone is loaded will teleport the player to the marker position.
+		of::file::FileId positionMarker;
+		glm::vec3 playerPos;
+
+		void test()
+		{
+			of::engine::GetModule<of::module::SceneManager>().loadWorldInstance(world, loadingScreen,
+				playerPos);
+		}
+
+	public:
+
+		Portal() = default;
+		~Portal() = default;
+
+		virtual Portal* copy() const override
+		{
+			return new Portal(*this);
+		}
+
+		virtual std::unique_ptr<Base> ucopy() const override
+		{
+			return std::make_unique<Portal>(*this);
+		}
+
+		void persist(of::object::ObjectSaveState*) override {};
+		void onReconstruct(of::object::ObjectSaveState*) override {};
+		void resolveReferences() override {};
+
+		of::common::uuid getType() const override
+		{
+			return typeId;
+		};
+
+		of::common::String getTypeName() const override
+		{
+			return "Portal";
+		};
+
+		static constexpr of::common::uuid typeId = "4eb8d991-d06e-4231-b3bc-82e4b5d5199d";
+	};
+}
