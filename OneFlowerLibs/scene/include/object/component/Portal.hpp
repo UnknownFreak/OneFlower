d93fdd44b8eb0c52533c3c55b65a643b43c093a4
UnknownFreak <UnknownFreak@users.noreply.github.com>
2023-08-06 10:30:18 +0200

    Extract code in portal.hpp to portal.cpp

--- OneFlowerLibs/scene/include/object/component/Portal.hpp
+++ OneFlowerLibs/scene/include/object/component/Portal.hpp
@@ -3,9 +3,6 @@
 #include <object/component/BaseComponent.hpp>
 #include <object/component/Transform.hpp>
 
-#include <module/sceneManager.hpp>
-#include <messaging/courier.hpp>
-
 #include <glm/glm.hpp>
 
 namespace of::object::component
@@ -13,63 +10,17 @@ namespace of::object::component
 	class Portal : public Base
 	{
 		
-		virtual void onMessage(const of::object::messaging::Message& message) override
-		{
-			using namespace of::object::messaging;
-			if (message.messageTopic == Topic::of(Topics::ON_COLLISION))
-			{
-				std::shared_ptr<GameObjectPtr> messageBody = std::dynamic_pointer_cast<GameObjectPtr>(message.messageBody);
-				// TODO: change tag to enum?
-				if (messageBody->go->tag == "player")
-				{
-					auto objectTrackingPos = messageBody->go->getShared<of::object::component::Transform>();
-					auto portalRef = attachedOn->getShared<Portal>();
+		virtual void onMessage(const of::object::messaging::Message& message) override;
 
-					auto& courier = of::engine::GetModule<of::messaging::Courier>();
-
-					// TODO: check if subscriber exists
-					//if (courier.hasSubscriber(of::messaging::Topic::Update, instanceId))
-					//	of::engine::GetModule<of::module::logger::OneLogger>().GetLogger("of::object::component::Portal").Info("Courier subscriber already exists, skipping!");
-					//	return;
-
-					courier.addSubscriber(
-						of::messaging::Topic::Update,
-						of::messaging::Subscriber(
-							instanceId, warrantyFromThis(),
-							[portalRef, objectTrackingPos] (const of::messaging::Message& msg)
-							{
-								float distance = glm::abs(glm::distance(objectTrackingPos->pos,
-								portalRef->mSelfTrackingPos->pos));
-								if (distance < portalRef->mTeleportDistance)
-								{
-									portalRef->teleport();
-								}
-								if (distance > portalRef->mGuiHintDistance)
-								{
-									auto& courier = of::engine::GetModule<of::messaging::Courier>();
-									courier.scheduleRemoval(of::messaging::Topic::Update, portalRef->instanceId);
-								}	
-							}
-						)
-					);
-				}
-			}
-		};
 
 		std::shared_ptr<of::object::component::Transform> mSelfTrackingPos;
 
 		float mTeleportDistance;
 		float mGuiHintDistance;
 
-		virtual void initialize() override
-		{
-			mSelfTrackingPos = attachedOn->getShared<of::object::component::Transform>();
-		}
-		virtual void deconstruct() override
-		{
-			auto courier = of::engine::GetModule<of::messaging::Courier>();
-			courier.removeSubscriber(of::messaging::Topic::Update, instanceId);
-		}
+		virtual void initialize() override;
+		virtual void deconstruct() override;
+
 
 		of::file::FileId world;
 		of::file::FileId loadingScreen;
@@ -77,11 +28,7 @@ namespace of::object::component
 		of::file::FileId positionMarker;
 		glm::vec3 teleportPos;
 
-		void teleport()
-		{
-			of::engine::GetModule<of::module::SceneManager>().loadWorldInstance(world, loadingScreen,
-				teleportPos);
-		}
+		void teleport();
 
 	public:
 
@@ -112,6 +59,6 @@ namespace of::object::component
 			return "Portal";
 		};
 
-		static constexpr of::common::uuid typeId = "4eb8d991-d06e-4231-b3bc-82e4b5d5199d";
+		static constexpr of::common::uuid typeId = "9f11a5ca-5f3c-4d2c-8cf2-7dc57a62503c";
 	};
 }
---
+++ OneFlowerLibs/scene/portal.cpp
@@ -0,0 +1,67 @@
+#include <object/component/portal.hpp>
+
+#include <module/sceneManager.hpp>
+#include <messaging/courier.hpp>
+
+namespace of::object::component
+{
+	void Portal::onMessage(const of::object::messaging::Message& message)
+	{
+		using namespace of::object::messaging;
+		if (message.messageTopic == Topic::of(Topics::ON_COLLISION))
+		{
+			std::shared_ptr<GameObjectPtr> messageBody = std::dynamic_pointer_cast<GameObjectPtr>(message.messageBody);
+			// TODO: change tag to enum?
+			if (messageBody->go->tag == "player")
+			{
+				auto objectTrackingPos = messageBody->go->getShared<of::object::component::Transform>();
+				auto portalRef = attachedOn->getShared<Portal>();
+
+				auto& courier = of::engine::GetModule<of::messaging::Courier>();
+
+				// TODO: check if subscriber exists
+				//if (courier.hasSubscriber(of::messaging::Topic::Update, instanceId))
+				//	of::engine::GetModule<of::module::logger::OneLogger>().GetLogger("of::object::component::Portal").Info("Courier subscriber already exists, skipping!");
+				//	return;
+
+				courier.addSubscriber(
+					of::messaging::Topic::Update,
+					of::messaging::Subscriber(
+						instanceId, warrantyFromThis(),
+						[portalRef, objectTrackingPos](const of::messaging::Message& msg)
+						{
+							float distance = glm::abs(glm::distance(objectTrackingPos->pos,
+							portalRef->mSelfTrackingPos->pos));
+							if (distance < portalRef->mTeleportDistance)
+							{
+								portalRef->teleport();
+							}
+							if (distance > portalRef->mGuiHintDistance)
+							{
+								auto& courier = of::engine::GetModule<of::messaging::Courier>();
+								courier.scheduleRemoval(of::messaging::Topic::Update, portalRef->instanceId);
+							}
+						}
+					)
+				);
+			}
+		}
+	}
+
+	void Portal::initialize()
+	{
+		mSelfTrackingPos = attachedOn->getShared<of::object::component::Transform>();
+	}
+
+	void Portal::deconstruct()
+	{
+		auto courier = of::engine::GetModule<of::messaging::Courier>();
+		courier.removeSubscriber(of::messaging::Topic::Update, instanceId);
+	}
+
+	void Portal::teleport()
+	{
+		of::engine::GetModule<of::module::SceneManager>().loadWorldInstance(world, loadingScreen,
+			teleportPos);
+	}
+}
