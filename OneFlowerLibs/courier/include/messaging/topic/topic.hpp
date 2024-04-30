b7717c964887147613e54bf14fba4b203f4e22bf
UnknownFreak <UnknownFreak@users.noreply.github.com>
2024-04-30 19:18:58 +0200

    Add example to what each topic is used for.

--- OneFlowerLibs/courier/include/messaging/topic/topic.hpp
+++ OneFlowerLibs/courier/include/messaging/topic/topic.hpp
@@ -7,18 +7,21 @@ namespace of::messaging
 	{
 		/*
 		Update is for generic object updates such as state or trigger changes.
-		Multi threaded, cannot interact with physics engine
+		Multi threaded, cannot interact with physics engine.
+		Examples: Status effect tick times, animations
 		*/
 		Update,
 		/*
 		Physics update is for dynamic object updates such as adding object torque/ force, or doing other 
 		physics updates.
 		Single threaded.
+		Examples: Physics forces applied, e.g growing / shrinking hitbox/ adding or removing forces
 		*/
 		PhysicsUpdate,
 		/*
 		Single Thread Update is for generic low priority updating logic. 
 		Single threaded, can interact with physics engine
+		Example: Physics raycast, e.g get mouse click on screen.
 		*/
 		SingleThreadUpdate,
 	};
